945c578d5bc6b24550c41f3a9540c71828427fe4
Reo Sahobby <reoshby@gmail.com>
2024-01-23 12:00:57 +0700

    Update tenorRepository.go

--- app/repository/tenorRepository.go
+++ app/repository/tenorRepository.go
@@ -3,20 +3,44 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"github.com/opentracing/opentracing-go"
+	"time"
+	"xyzApp/app/customError"
 	"xyzApp/app/model/entity"
 )
 
-type TonerRepository struct {
+type TenorRepository struct {
 	db *sql.DB
 }
 
 // func provider
 func NewTenorRepository(db *sql.DB) ITenorRepository {
-	return &TonerRepository{db}
+	return &TenorRepository{db}
 }
 
 // method insert data tenor
-func (t *TonerRepository) Insert(ctx context.Context, input *entity.Tenor) (*entity.Tenor, error) {
-	//TODO implement me
-	panic("implement me")
+func (t *TenorRepository) Insert(ctx context.Context, input *entity.Tenor) (*entity.Tenor, error) {
+	span, ctxTracing := opentracing.StartSpanFromContext(ctx, "TenorRepository Insert")
+	defer span.Finish()
+
+	// prepare query
+	statement, err := t.db.PrepareContext(ctxTracing, "INSERT INTO tenor (nik, bulan, start_date, end_date, tenor, created_at, updated_at) VALUE (?, ?, ?, ?, ?, ?, ?)")
+	defer statement.Close()
+	if err != nil {
+		return nil, customError.NewInternalSeverError(err.Error())
+	}
+
+	// execute
+	result, err := statement.ExecContext(ctxTracing, input.Nik, input.Bulan, input.StartDate, input.EndDate, input.Tenor, time.Now(), time.Now())
+	if err != nil {
+		return nil, customError.NewInternalSeverError(err.Error())
+	}
+
+	// check row affeected
+	if row, _ := result.RowsAffected(); row == 0 {
+		return nil, customError.NewInternalSeverError("cant insert data tenor")
+	}
+
+	// success insert
+	return input, nil
 }
